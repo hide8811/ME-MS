638cf9f53b78533c361f1907b7c4d87bf9181b24
hide8811 <ikuyedih18@gmail.com>
2020-10-17 13:07:07 +0900

    Add: [customer] indexアクション テスト追加

--- backend/spec/requests/customers_request_spec.rb
+++ backend/spec/requests/customers_request_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Customers', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/customers/index'
+  describe 'GET #index' do
+    it 'リクエストが成功すること' do
+      get '/customers'
       expect(response).to have_http_status(:success)
+    end
+
+    it '全てのcustomersテーブルのデータを取得できていること' do
+      create_list(:customer, 10)
+
+      get '/customers'
+      json = JSON.parse(response.body)
+
+      expect(json.length).to eq 10
     end
   end
 
