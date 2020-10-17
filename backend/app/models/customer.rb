3276841229e3cd646398a37cfe7a1cbc3f151735
hide8811 <ikuyedih18@gmail.com>
2020-10-17 12:16:26 +0900

    Add: [customer] バリデーション追加

--- backend/app/models/customer.rb
+++ backend/app/models/customer.rb
@@ -1,2 +1,23 @@
 class Customer < ApplicationRecord
+  with_options presence: true do
+    validates :age
+    validates :date
+    validates :time
+    validates :course
+    validates :extended_time
+    validates :deep_lymph
+  end
+
+  validates :option, inclusion: { in: [true, false] }
+  validates :cosplay, inclusion: { in: [true, false] }
+
+  with_options if: :option_false? do
+    validates :cosplay, format: { with: /\Afalse\z/ }
+    validates :extended_time, format: { with: /\A--\z/ }
+    validates :deep_lymph, format: { with: /\A--\z/ }
+  end
+
+  def option_false?
+    option == false
+  end
 end
