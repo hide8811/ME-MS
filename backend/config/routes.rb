a293e0738d91f81aad82382cc593fc05d52113c4
hide8811 <ikuyedih18@gmail.com>
2020-10-17 12:42:09 +0900

    Improve: [customer] ルーティングの記述を改善

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get 'customers/index'
-  get 'customers/create'
-  get 'customers/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :customers, only: %i[index create show]
 end
