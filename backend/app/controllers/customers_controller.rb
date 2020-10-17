54e2d937f6a7024361279ebfcdce23f687d215c8
hide8811 <ikuyedih18@gmail.com>
2020-10-17 13:19:48 +0900

    Add: [customer] createアクション 追加

--- backend/app/controllers/customers_controller.rb
+++ backend/app/controllers/customers_controller.rb
@@ -4,7 +4,21 @@ class CustomersController < ApplicationController
     render json: @customers
   end
 
-  def create; end
+  def create
+    @customer = Customer.new(customer_params)
+
+    if @customer.save
+      render json: @customer, status: :created
+    else
+      render json: @customer, status: :unprocessable_entity
+    end
+  end
 
   def show; end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:age, :date, :time, :course, :option, :cosplay, :extended_time, :deep_lymph)
+  end
 end
