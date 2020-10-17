c191e1091d1e92b1623f7e79ac191c915cad7eca
hide8811 <ikuyedih18@gmail.com>
2020-10-17 11:44:04 +0900

    Add: [cusromer] NOT NULL制約を追加

--- backend/db/migrate/20201017022639_create_customers.rb
+++ backend/db/migrate/20201017022639_create_customers.rb
@@ -1,14 +1,14 @@
 class CreateCustomers < ActiveRecord::Migration[6.0]
   def change
     create_table :customers do |t|
-      t.string :age
-      t.date :date
-      t.time :time
-      t.string :course
-      t.boolean :option
-      t.boolean :cosplay
-      t.string :extended_time
-      t.string :deep_lymph
+      t.string :age, null: false
+      t.date :date, null: false
+      t.time :time, null: false
+      t.string :course, null: false
+      t.boolean :option, null: false, default: false
+      t.boolean :cosplay, null: false, default: false
+      t.string :extended_time, null: false
+      t.string :deep_lymph, null: false
 
       t.timestamps
     end
