b7abd0067445f45c8009f481cbca107a44dd42f7
KohnoTaiyo <taiyowork66@gmail.com>
2020-10-23 17:04:18 +0900

    課題大杉ワロタ・・・

--- frontend/src/pages/admins/_id.tsx
+++ frontend/src/pages/admins/_id.tsx
@@ -1,11 +1,11 @@
 import React,{useState,useEffect} from 'react'
-// import styled from 'styled-components'
+import styled from 'styled-components'
 import axios from 'axios'
-import ageFilter,{ ageFilterArray } from '../../controls/percentAge'
+import { FilterAge, initialAgeFilter } from '../../controls/FilterAge'
 
 const Month:React.FC = (props:any) => {
   
-  const urlParams = props.match.params.id
+  // const urlParams = props.match.params.id
   const [datas, getDatas] = useState([])
 
   useEffect(() => {
@@ -16,54 +16,52 @@ const Month:React.FC = (props:any) => {
   },[])
   
   datas.map((val:any)=>{
-    ageFilter(val.age)
+    // FilterAge > controls
+    FilterAge(val.age)
   })
 
-  const numberCustomers = [
-    ageFilterArray.age10.length,
-    ageFilterArray.age20.length,
-    ageFilterArray.age25.length,
-    ageFilterArray.age30.length,
-    ageFilterArray.age35.length,
-    ageFilterArray.age40.length,
-    ageFilterArray.age45.length,
-    ageFilterArray.age50.length,
-    ageFilterArray.age55.length,
-    ageFilterArray.age60.length,
-    ageFilterArray.age65.length,
-    ageFilterArray.age70.length
-  ]
-  const divisionNumberCustomers = numberCustomers.map(val => val / 2)
+  const numberCustomersArray:Array<number> = []
+  const numberCustomers = () => {
+    // initialAgeFilter > controls
+    for(let i = 0; i < initialAgeFilter.length; i++){
+      numberCustomersArray.push(initialAgeFilter[i].agesArray.length)
+    }
+  }
+  numberCustomers()
+
+  const divisionNumberCustomers = numberCustomersArray.map(val => val / 2)
   const percentabNumberCustomers = divisionNumberCustomers.map(val => val / datas.length * 100)
-  const OrganizeNumberCustomers = percentabNumberCustomers.map(val => Math.round(val * 10) / 10)
+  const organizeNumberCustomers = percentabNumberCustomers.map(val => Math.round(val * 10) / 10)
 
-  const agesVal = [
-    '10代',
-    '20代前半',
-    '20代後半',
-    '30代前半',
-    '30代後半',
-    '40代前半',
-    '40代後半',
-    '50代前半',
-    '50代後半',
-    '60代前半',
-    '60代後半',
-    '70代以上',
-  ]
-
-  const showNumberCustomers = OrganizeNumberCustomers.map((val,index) => {
-    return(
-    <div key={index}>{agesVal[index]} : {val}%</div>
+  const showNumberCustomers = organizeNumberCustomers.map((val,index) => {
+    // initialAgeFilter > controls
+    let valval = String({val})
+    const cok = {
+      width: {valval}, background: "#000"
+    }
+    return (
+    <div className='flex-box' key={index}>
+      <pre>{ initialAgeFilter[index].caseTitle} : </pre>
+      <pre> {val}%</pre>
+    </div>
     )
   })
 
   return(
-    <div>
-      {urlParams}
+    <CONTAINER>
+      {/* {urlParams} */}
       {showNumberCustomers}
-    </div>
+    </CONTAINER>
   )
 }
+
+const CONTAINER = styled.div`
+  padding: 10px;
+  .flex-box {
+    display: flex;
+    margin-bottom: 10px;
+    font-size: 18px;
+  }
+`;
 
 export default Month
