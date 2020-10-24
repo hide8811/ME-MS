4828de4dbaa801e55f19c8aa77786620107c5c8b
KohnoTaiyo <taiyowork66@gmail.com>
2020-10-24 13:30:40 +0900

    async awaitで解決

--- frontend/src/controls/FilterAge.tsx
+++ frontend/src/controls/FilterAge.tsx
@@ -1,58 +1,100 @@
-export const initialAgeFilter:any = [
-  {caseTitle: '10代', agesArray:[]},
-  {caseTitle: '20代前半', agesArray:[]},
-  {caseTitle: '20代後半', agesArray:[]},
-  {caseTitle: '30代前半', agesArray:[]},
-  {caseTitle: '30代後半', agesArray:[]},
-  {caseTitle: '40代前半', agesArray:[]},
-  {caseTitle: '40代後半', agesArray:[]},
-  {caseTitle: '50代前半', agesArray:[]},
-  {caseTitle: '50代後半', agesArray:[]},
-  {caseTitle: '60代前半', agesArray:[]},
-  {caseTitle: '60代後半', agesArray:[]},
-  {caseTitle: '70代以上', agesArray:[]}
-]
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
 
-export const FilterAge = (users:string) => {  
-  switch(users) {
-    case '10代':
-      initialAgeFilter[0].agesArray.push(1)
-      break;
-    case '20代前半':
-      initialAgeFilter[1].agesArray.push(1)
-      break;
-    case '20代後半':
-      initialAgeFilter[2].agesArray.push(1)
-      break;
-    case '30代前半':
-      initialAgeFilter[3].agesArray.push(1)
-      break;
-    case '30代後半':
-      initialAgeFilter[4].agesArray.push(1)
-      break;
-    case '40代前半':
-      initialAgeFilter[5].agesArray.push(1)
-      break;
-    case '40代後半':
-      initialAgeFilter[6].agesArray.push(1)
-      break;
-    case '50代前半':
-      initialAgeFilter[7].agesArray.push(1)
-      break;
-    case '50代後半':
-      initialAgeFilter[8].agesArray.push(1)
-      break;
-    case '60代前半':
-      initialAgeFilter[9].agesArray.push(1)
-      break;
-    case '60代後半':
-      initialAgeFilter[10].agesArray.push(1)
-      break;
-    case '70代以上':
-      initialAgeFilter[11].agesArray.push(1)
-      break;
-    default:
-      console.log('run')
-      return null
+const FilterAge = () => {
+
+  const [datas, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('http://localhost:3000/customers')
+      setData(result.data)
+    }
+    fetchData()
+  },[])
+
+  const initialAgeFilter:any = [
+    {caseTitle: '10代', agesArray:[]},
+    {caseTitle: '20代前半', agesArray:[]},
+    {caseTitle: '20代後半', agesArray:[]},
+    {caseTitle: '30代前半', agesArray:[]},
+    {caseTitle: '30代後半', agesArray:[]},
+    {caseTitle: '40代前半', agesArray:[]},
+    {caseTitle: '40代後半', agesArray:[]},
+    {caseTitle: '50代前半', agesArray:[]},
+    {caseTitle: '50代後半', agesArray:[]},
+    {caseTitle: '60代前半', agesArray:[]},
+    {caseTitle: '60代後半', agesArray:[]},
+    {caseTitle: '70代以上', agesArray:[]}
+  ]
+
+  const FilterCase = (users:string) => {  
+    switch(users) {
+      case '10代':
+        initialAgeFilter[0].agesArray.push(1)
+        break;
+      case '20代前半':
+        initialAgeFilter[1].agesArray.push(1)
+        break;
+      case '20代後半':
+        initialAgeFilter[2].agesArray.push(1)
+        break;
+      case '30代前半':
+        initialAgeFilter[3].agesArray.push(1)
+        break;
+      case '30代後半':
+        initialAgeFilter[4].agesArray.push(1)
+        break;
+      case '40代前半':
+        initialAgeFilter[5].agesArray.push(1)
+        break;
+      case '40代後半':
+        initialAgeFilter[6].agesArray.push(1)
+        break;
+      case '50代前半':
+        initialAgeFilter[7].agesArray.push(1)
+        break;
+      case '50代後半':
+        initialAgeFilter[8].agesArray.push(1)
+        break;
+      case '60代前半':
+        initialAgeFilter[9].agesArray.push(1)
+        break;
+      case '60代後半':
+        initialAgeFilter[10].agesArray.push(1)
+        break;
+      case '70代以上':
+        initialAgeFilter[11].agesArray.push(1)
+        break;
+      default:
+        return null
+    }
   }
+  datas.map((val:any)=>{
+    FilterCase(val.age)
+  })
+  const numberCustomersArray:Array<number> = []
+
+  for(let i = 0; i < initialAgeFilter.length; i++){
+    numberCustomersArray.push(initialAgeFilter[i].agesArray.length)
+  }
+  const organizeNumberCustomers = numberCustomersArray
+  .map(val => Math.round(val / datas.length * 1000) / 10)
+
+  const showNumberCustomers = organizeNumberCustomers.map((val,index) => {
+    return (
+    <div className='flex-box' key={index}>
+      <pre>{ initialAgeFilter[index].caseTitle} : </pre>
+      <pre> {val}%</pre>
+    </div>
+    )
+  })
+  
+  return(
+  <>
+    {showNumberCustomers}
+  </>
+  )
 }
+
+export default FilterAge
--- frontend/src/pages/admins/_id.tsx
+++ frontend/src/pages/admins/_id.tsx
@@ -1,56 +1,13 @@
-import React,{useState,useEffect} from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import axios from 'axios'
-import { FilterAge, initialAgeFilter } from '../../controls/FilterAge'
+import FilterAge from '../../controls/FilterAge'
 
 const Month:React.FC = (props:any) => {
-  
-  // const urlParams = props.match.params.id
-  const [datas, getDatas] = useState([])
-
-  useEffect(() => {
-    // const params = { getParams: '2020-' + urlParams }
-    // axios.get('http://localhost:3000/customers',{ params })
-    axios.get('http://localhost:3000/customers')
-    .then(res => getDatas(res.data))
-  },[])
-  
-  datas.map((val:any)=>{
-    // FilterAge > controls
-    FilterAge(val.age)
-  })
-
-  const numberCustomersArray:Array<number> = []
-  const numberCustomers = () => {
-    // initialAgeFilter > controls
-    for(let i = 0; i < initialAgeFilter.length; i++){
-      numberCustomersArray.push(initialAgeFilter[i].agesArray.length)
-    }
-  }
-  
-  numberCustomers()
-  const divisionNumberCustomers = numberCustomersArray.map(val => val / 2)
-  const percentabNumberCustomers = divisionNumberCustomers.map(val => val / datas.length * 100)
-  const organizeNumberCustomers = percentabNumberCustomers.map(val => Math.round(val * 10) / 10)
-
-  const showNumberCustomers = organizeNumberCustomers.map((val,index) => {
-    // initialAgeFilter > controls
-    // let valval = String({val})
-    // const cok = {
-    //   width: {valval}, background: "#000"
-    // }
-    return (
-    <div className='flex-box' key={index}>
-      <pre>{ initialAgeFilter[index].caseTitle} : </pre>
-      <pre> {val}%</pre>
-    </div>
-    )
-  })
 
   return(
     <CONTAINER>
       {/* {urlParams} */}
-      {showNumberCustomers}
+      <FilterAge />
     </CONTAINER>
   )
 }
